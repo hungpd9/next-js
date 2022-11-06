8e0962f51f335fa0562287039246737b60ad636b
HungPD <hung.toyo.nikkoku@gmail.com>
2022-11-07 00:16:24 +0700

    postcss + tailwind css

---
+++ pages/posts/first.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+    return (<>
+        <Head>
+            <title>First post</title>
+        </Head>
+        <div className="px-3">
+            <h2>first post</h2>
+        </div>
+
+    </>);
+}
